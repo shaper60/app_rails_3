281723b25123d5fefe4d3eacbf0c5d66c98f68aa
shaper60 <shaper60@gmail.com>
2015-04-19 18:25:42 +0900

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,10 +62,19 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      is { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
@@ -123,43 +132,55 @@ describe "User pages" do
         it { should have_selector('div.alert.alert-success', text: "Welcome") }
       end
     end
+  end
 
-    describe "edit" do
-      let(:user) { FactoryGirl.create(:user) }
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
       before do
-        sign_in user
-        visit edit_user_path(user)
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
       end
 
-      describe "page" do
-        it { should have_content("Update your profile") }
-        it { should have_title("Edit user") }
-        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success')}
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+          password_confirmation: user.password } }
       end
-
-      describe "with invalid information" do
-        before { click_button "Save changes" }
-
-        it { should have_content('error') }
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
       end
-
-      describe "with valid information" do
-        let(:new_name) { "New Name" }
-        let(:new_email) { "new@example.com" }
-        before do
-          fill_in "Name", with: new_name
-          fill_in "Email", with: new_email
-          fill_in "Password", with: user.password
-          fill_in "Confirm Password", with: user.password
-          click_button "Save changes"
-        end
-
-        it { should have_title(new_name) }
-        it { should have_selector('div.alert.alert-success')}
-        it { should have_link('Sign out', href: signout_path) }
-        specify { expect(user.reload.name).to eq new_name }
-        specify { expect(user.reload.email).to eq new_email }
-      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
