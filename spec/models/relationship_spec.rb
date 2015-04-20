ce9022e6d260067d683f2f8fed81b5117cd8d6a0
shaper60 <shaper60@gmail.com>
2015-04-20 11:08:01 +0900

    Add user following

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Relationship do
+
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { FactoryGirl.relationship.build(followed_id: followed.id) }
+
+  subject { relationship }
+  it { should be_valid }
+
+  describe "follower methods" do
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should eq follower }
+    its(:followed) { should eq followed }
+  end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+end
